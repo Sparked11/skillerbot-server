05dba47a24c67edccbbcff2573eb072358269ffa
Deb <talk2debaditya@gmail.com>
2025-07-29 08:34:08 -0500

    Add default route

--- server.js
+++ server.js
@@ -43,3 +43,7 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`✅ SkillerBot server is running on http://localhost:${PORT}`);
 });
+
+app.get('/', (req, res) => {
+  res.send('✅ SkillerBot server is running!');
+});
